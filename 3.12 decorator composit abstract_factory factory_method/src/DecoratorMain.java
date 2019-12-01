bd7b9161c64fb16d8885907b8fa0c74886ab0903
Wojtek Fornagiel <wojtasfor@gmail.com>
2019-12-01 17:11:27 +0100

    fix main formating

--- 3.12 decorator composit abstract_factory factory_method/src/DecoratorMain.java
+++ 3.12 decorator composit abstract_factory factory_method/src/DecoratorMain.java
@@ -3,12 +3,20 @@ public class DecoratorMain {
 
 	public static void main(String[] args) {
 		Komponent mojePotw;
-		mojePotw = Konfiguracja.pobierzPotwierdzenie(new DekoratorNaglowka1(new DekoratorStopki2(new DekoratorStopki1(new Potwierdzenie()))));
+		mojePotw = Konfiguracja.pobierzPotwierdzenie(
+				new DekoratorNaglowka1(
+				new DekoratorStopki2(
+				new DekoratorStopki1(
+				new Potwierdzenie()))));
 		mojePotw.drukuj();
 		
 		System.out.println();
 		
-		mojePotw = Konfiguracja.pobierzPotwierdzenie(new DekoratorNaglowka1(new DekoratorNaglowka2(new DekoratorStopki2(new Potwierdzenie()))));
+		mojePotw = Konfiguracja.pobierzPotwierdzenie(
+				new DekoratorNaglowka1(
+				new DekoratorNaglowka2(
+				new DekoratorStopki2(
+				new Potwierdzenie()))));
 		mojePotw.drukuj();
 	}
 
